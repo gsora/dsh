a0787bf19fc2ed69968a4053b106af39cf9371a5
Gianguido Sorà <gianguidorama@gmail.com>
2014-03-17 22:16:00 +0100

    working, or it seems to

---
+++ dsh.py
@@ -0,0 +1,60 @@
+import subprocess
+import signal 
+import os
+
+def signal_handler(signal, frame):
+    pass
+
+def menu():
+    stdinShell = input("dsh> ")
+    if stdinShell == "exit":
+        exit()
+    
+    if stdinShell == "clear":
+        subprocess.call("clear")
+        return True
+        
+    if stdinShell == "help":
+        print("Dumb Shell: the dumbest shell you'll find on the net. Period.")
+        return True
+    commandList = stdinShell.split(' ')
+    
+    argsString = ""
+    firstIteration = True
+    for i in commandList:
+        if len(commandList) == 1:
+            argsString = "no arguments given"
+            break
+        if i == commandList[0]:
+            pass
+        else:
+            if firstIteration == True:
+                argsString = i
+                firstIteration = False
+            else:
+                argsString = argsString + ", " + i
+                
+    if(len(commandList) == 1 and commandList[0] == ""):
+        pass
+    else:
+        try:
+            if commandList[0] == "cd":
+                try:
+                    os.chdir(commandList[1])
+                    return True
+                except IndexError:
+                    os.chdir(os.path.expanduser("~"))
+                    return True
+            spawnedProc = subprocess.Popen(commandList, shell=False); spawnedProc.wait()
+        except KeyboardInterrupt:
+            spawnedProc.kill()
+        except FileNotFoundError:
+            print("Error --> {}: no such file or directory".format(commandList[0]))
+    
+def main():
+    signal.signal(signal.SIGINT, signal_handler)
+    while(True):
+        menu()
+
+if __name__ == "__main__":
+    main()
