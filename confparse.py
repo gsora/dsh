40dd4eada520fbae135f593b4f66005a36384621
Gianguido Sorà <gianguidorama@gmail.com>
2014-03-18 22:24:08 +0100

    now parsing history file path

--- confparse.py
+++ confparse.py
@@ -1,9 +1,9 @@
-from time import strftime
-from socket import gethostname
-from os import getcwd, path
-from getpass import getuser
-
 def getPS1():
+    from time import strftime
+    from socket import gethostname
+    from os import getcwd, path
+    from getpass import getuser
+    
     configFilePath = path.expanduser("~/.dshrc")
 
     currentTime = strftime("%X")
@@ -18,3 +18,15 @@ def getPS1():
             pass
         elif line[:4] == "PS1=":
             return(line.replace("@time", currentTime).replace("@date", currentDate).replace("@host", hostname).replace("@pwd", curDir).replace("@user", username).rstrip('\n')[4:])
+
+def getHistFile():
+    from os import path
+    
+    configFilePath = path.expanduser("~/.dshrc")
+
+    configFile = open(configFilePath, 'r+')
+    for line in configFile:
+        if line[:1] is "#":
+            pass
+        elif line[:5] == "HIST=":
+            return(line[5:])
